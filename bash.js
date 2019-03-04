016d37d6c27209e1413c03cc3174996b77c23d12
Jon Kurinsky <jkurinsky@gmail.com>
2019-03-04 11:43:49 -0600

    finished ls, pwd, working on cat

--- bash.js
+++ bash.js
@@ -1,16 +1,24 @@
+let pwdFunc = require('./pwd');
+let lsFunc = require('./ls');
+let catFunc = require('./cat');
+
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-    let cmdString = data.toString().split(" ");
-    let cmd = cmdString[0];
-    console.log(cmd)
-    if (cmd == 'pwd'){
-      console.log('test')
-      let pwd = process.cwd();
-      console.log(pwd)
-    }
-  //process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt >');
+process.stdin.on('data', data => {
+  // command pieces
+  let cmdString = data.toString().split(' ');
+  let cmd = cmdString[0].trim();
+  let args = data
+    .toString()
+    .split(' ')
+    .slice(1);
 
-})
-
+  // code to run commands
+  if (cmd === 'pwd') {
+    pwdFunc();
+  } else if (cmd === 'ls') {
+    lsFunc();
+  } else if (cmd === 'cat') {
+    catFunc(args[0]);
+  }
+});
